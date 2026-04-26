138693ba84aa724e075279299143d4902033f707
vanxey <82112436+vanxey@users.noreply.github.com>
2026-04-26 19:09:57 +0200

    feat: add basic server structure

---
+++ src/server/main.js
@@ -0,0 +1,25 @@
+import { createServer }  from 'node:https';
+import { readFileSync } from 'node:fs';
+import { setHeaders } from './security.js';
+
+const sslOptions = {
+    key: readFileSync('./certs/private-key.pem'),
+    cert: readFileSync('./certs/certificate.pem'),
+
+    minVersion: 'TLSv1.3',
+    maxVersion: 'TLSv1.3'
+};
+
+const server = createServer(sslOptions, (req, res) => {
+
+});
+
+server.on('error', (error) => {
+  console.error('Server error:', error);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at https://localhost:${PORT}`);
+  console.log('Press Ctrl+C to stop the server');
+});
